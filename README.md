9e43988784ae674865cc664486b15791ce1d91c8
Michał Talaga <michal.talaga.programming@gmail.com>
2024-01-30 23:08:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BOOTSTRAP-PROJECT-1
+Projekt szkolny na zaliczenie przediotu Zaawansowane Aplikacje Internetowe
---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+//simple react
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// ReactDOM.render("Hello World", document.getElementById('app'));
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
